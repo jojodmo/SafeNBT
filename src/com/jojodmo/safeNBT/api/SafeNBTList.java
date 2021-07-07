68742cab2cc752447ebdc8677346c025161c2993
Domenico <admin@jojodmo.com>
2021-07-07 13:01:31 -0700

    Update SafeNBTList.java

--- src/com/jojodmo/safeNBT/api/SafeNBTList.java
+++ src/com/jojodmo/safeNBT/api/SafeNBTList.java
@@ -1,6 +1,6 @@
 /*
 
-   Copyright 2019 jojodmo
+   Copyright 2019-2021 jojodmo
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
